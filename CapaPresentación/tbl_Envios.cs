09209ed73ccdfc2148702745ad81c002ad9e05f3
AugustoMoranUMG <gmroana2@miumg.edu.gt>
2025-10-20 22:35:36 -0600

    Creación de tablas: VentasDetalle PagosVentas Envios Proveedores Insumos Inventarios

--- CapaPresentación/tbl_Envios.cs
+++ CapaPresentación/tbl_Envios.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CapaPresentación
 {
-    public partial class Form1: Form
+    public partial class tbl_Envios: Form
     {
-        public Form1()
+        public tbl_Envios()
         {
             InitializeComponent();
         }
