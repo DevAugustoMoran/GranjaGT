2f6b233fc49cd03f199277f1bfe7663bc67ccd80
AugustoMoranUMG <gmroana2@miumg.edu.gt>
2025-10-21 21:46:48 -0600

    Diseño de formulario

--- CapaPresentación/frmVentasDetalle.Designer.cs
+++ CapaPresentación/frmVentasDetalle.Designer.cs
@@ -28,13 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvConsumos = new System.Windows.Forms.DataGridView();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboxEstado = new System.Windows.Forms.ComboBox();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.dtpFechaConsumo = new System.Windows.Forms.DateTimePicker();
+            this.cboxCodigoServicio = new System.Windows.Forms.ComboBox();
+            this.cboxCodigoReserva = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigoConsumo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsumos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvConsumos
+            // 
+            this.dgvConsumos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvConsumos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsumos.Location = new System.Drawing.Point(12, 304);
+            this.dgvConsumos.Name = "dgvConsumos";
+            this.dgvConsumos.ReadOnly = true;
+            this.dgvConsumos.RowHeadersWidth = 51;
+            this.dgvConsumos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvConsumos.Size = new System.Drawing.Size(1240, 365);
+            this.dgvConsumos.TabIndex = 71;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Calisto MT", 20F, System.Drawing.FontStyle.Bold);
+            this.label11.Location = new System.Drawing.Point(461, 28);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(305, 31);
+            this.label11.TabIndex = 75;
+            this.label11.Text = "DETALLES DE VENTA";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Calisto MT", 14F);
+            this.label10.Location = new System.Drawing.Point(942, 35);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(117, 22);
+            this.label10.TabIndex = 73;
+            this.label10.Text = "Fecha actual:";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Calisto MT", 14F);
+            this.lblFecha.Location = new System.Drawing.Point(1065, 35);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(137, 22);
+            this.lblFecha.TabIndex = 72;
+            this.lblFecha.Text = "Imprimir Fecha";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cboxEstado);
+            this.groupBox1.Controls.Add(this.lblMonto);
+            this.groupBox1.Controls.Add(this.dtpFechaConsumo);
+            this.groupBox1.Controls.Add(this.cboxCodigoServicio);
+            this.groupBox1.Controls.Add(this.cboxCodigoReserva);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCodigoConsumo);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1240, 212);
+            this.groupBox1.TabIndex = 74;
+            this.groupBox1.TabStop = false;
+            // 
+            // cboxEstado
+            // 
+            this.cboxEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboxEstado.FormattingEnabled = true;
+            this.cboxEstado.Items.AddRange(new object[] {
+            "Solicitado",
+            "Progreso",
+            "Finalizado",
+            "Cancelado",
+            "Facturado"});
+            this.cboxEstado.Location = new System.Drawing.Point(855, 174);
+            this.cboxEstado.Name = "cboxEstado";
+            this.cboxEstado.Size = new System.Drawing.Size(154, 23);
+            this.cboxEstado.TabIndex = 68;
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Location = new System.Drawing.Point(857, 39);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(10, 13);
+            this.lblMonto.TabIndex = 67;
+            this.lblMonto.Text = "-";
+            // 
+            // dtpFechaConsumo
+            // 
+            this.dtpFechaConsumo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaConsumo.Location = new System.Drawing.Point(855, 102);
+            this.dtpFechaConsumo.Name = "dtpFechaConsumo";
+            this.dtpFechaConsumo.Size = new System.Drawing.Size(154, 20);
+            this.dtpFechaConsumo.TabIndex = 66;
+            // 
+            // cboxCodigoServicio
+            // 
+            this.cboxCodigoServicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboxCodigoServicio.FormattingEnabled = true;
+            this.cboxCodigoServicio.Location = new System.Drawing.Point(271, 170);
+            this.cboxCodigoServicio.Name = "cboxCodigoServicio";
+            this.cboxCodigoServicio.Size = new System.Drawing.Size(156, 23);
+            this.cboxCodigoServicio.TabIndex = 64;
+            // 
+            // cboxCodigoReserva
+            // 
+            this.cboxCodigoReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboxCodigoReserva.FormattingEnabled = true;
+            this.cboxCodigoReserva.Location = new System.Drawing.Point(271, 98);
+            this.cboxCodigoReserva.Name = "cboxCodigoReserva";
+            this.cboxCodigoReserva.Size = new System.Drawing.Size(156, 23);
+            this.cboxCodigoReserva.TabIndex = 58;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calisto MT", 14F);
+            this.label7.Location = new System.Drawing.Point(666, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(146, 22);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Fecha Consumo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calisto MT", 14F);
+            this.label3.Location = new System.Drawing.Point(666, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 22);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Monto:";
+            // 
+            // txtCodigoConsumo
+            // 
+            this.txtCodigoConsumo.Location = new System.Drawing.Point(271, 37);
+            this.txtCodigoConsumo.Name = "txtCodigoConsumo";
+            this.txtCodigoConsumo.ReadOnly = true;
+            this.txtCodigoConsumo.Size = new System.Drawing.Size(156, 20);
+            this.txtCodigoConsumo.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calisto MT", 14F);
+            this.label5.Location = new System.Drawing.Point(666, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Estado:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calisto MT", 14F);
+            this.label4.Location = new System.Drawing.Point(98, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Codigo Servicio:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calisto MT", 14F);
+            this.label2.Location = new System.Drawing.Point(98, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Codigo Reserva:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calisto MT", 14F);
+            this.label1.Location = new System.Drawing.Point(98, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Codigo Consumo:";
+            // 
+            // frmVentasDetalle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.dgvConsumos);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmVentasDetalle";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsumos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvConsumos;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cboxEstado;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.DateTimePicker dtpFechaConsumo;
+        private System.Windows.Forms.ComboBox cboxCodigoServicio;
+        private System.Windows.Forms.ComboBox cboxCodigoReserva;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigoConsumo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
