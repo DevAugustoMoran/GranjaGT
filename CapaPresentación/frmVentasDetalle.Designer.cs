50688ccb27bb8c07a229b522ea7cd3c04d0afd5e
Fernando <ferchogfs17qgmail.com>
2025-10-21 22:25:36 -0600

    Primer formulario

--- CapaPresentación/frmVentasDetalle.Designer.cs
+++ CapaPresentación/frmVentasDetalle.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dgvConsumos = new System.Windows.Forms.DataGridView();
             this.label11 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
@@ -45,6 +46,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dgvConsumos)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -53,21 +55,23 @@
             // 
             this.dgvConsumos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvConsumos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvConsumos.Location = new System.Drawing.Point(12, 304);
+            this.dgvConsumos.Location = new System.Drawing.Point(16, 374);
+            this.dgvConsumos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvConsumos.Name = "dgvConsumos";
             this.dgvConsumos.ReadOnly = true;
             this.dgvConsumos.RowHeadersWidth = 51;
             this.dgvConsumos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvConsumos.Size = new System.Drawing.Size(1240, 365);
+            this.dgvConsumos.Size = new System.Drawing.Size(1653, 449);
             this.dgvConsumos.TabIndex = 71;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Calisto MT", 20F, System.Drawing.FontStyle.Bold);
-            this.label11.Location = new System.Drawing.Point(461, 28);
+            this.label11.Location = new System.Drawing.Point(615, 34);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(305, 31);
+            this.label11.Size = new System.Drawing.Size(397, 39);
             this.label11.TabIndex = 75;
             this.label11.Text = "DETALLES DE VENTA";
             // 
@@ -75,9 +79,10 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Calisto MT", 14F);
-            this.label10.Location = new System.Drawing.Point(942, 35);
+            this.label10.Location = new System.Drawing.Point(1256, 43);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(117, 22);
+            this.label10.Size = new System.Drawing.Size(149, 28);
             this.label10.TabIndex = 73;
             this.label10.Text = "Fecha actual:";
             // 
@@ -85,9 +90,10 @@
             // 
             this.lblFecha.AutoSize = true;
             this.lblFecha.Font = new System.Drawing.Font("Calisto MT", 14F);
-            this.lblFecha.Location = new System.Drawing.Point(1065, 35);
+            this.lblFecha.Location = new System.Drawing.Point(1420, 43);
+            this.lblFecha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(137, 22);
+            this.lblFecha.Size = new System.Drawing.Size(174, 28);
             this.lblFecha.TabIndex = 72;
             this.lblFecha.Text = "Imprimir Fecha";
             // 
@@ -105,9 +111,11 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 86);
+            this.groupBox1.Location = new System.Drawing.Point(16, 105);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1240, 212);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1653, 261);
             this.groupBox1.TabIndex = 74;
             this.groupBox1.TabStop = false;
             // 
@@ -121,53 +129,59 @@
             "Finalizado",
             "Cancelado",
             "Facturado"});
-            this.cboxEstado.Location = new System.Drawing.Point(855, 174);
+            this.cboxEstado.Location = new System.Drawing.Point(1140, 214);
+            this.cboxEstado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cboxEstado.Name = "cboxEstado";
-            this.cboxEstado.Size = new System.Drawing.Size(154, 23);
+            this.cboxEstado.Size = new System.Drawing.Size(204, 26);
             this.cboxEstado.TabIndex = 68;
             // 
             // lblMonto
             // 
             this.lblMonto.AutoSize = true;
-            this.lblMonto.Location = new System.Drawing.Point(857, 39);
+            this.lblMonto.Location = new System.Drawing.Point(1143, 48);
+            this.lblMonto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMonto.Name = "lblMonto";
-            this.lblMonto.Size = new System.Drawing.Size(10, 13);
+            this.lblMonto.Size = new System.Drawing.Size(11, 16);
             this.lblMonto.TabIndex = 67;
             this.lblMonto.Text = "-";
             // 
             // dtpFechaConsumo
             // 
             this.dtpFechaConsumo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpFechaConsumo.Location = new System.Drawing.Point(855, 102);
+            this.dtpFechaConsumo.Location = new System.Drawing.Point(1140, 126);
+            this.dtpFechaConsumo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtpFechaConsumo.Name = "dtpFechaConsumo";
-            this.dtpFechaConsumo.Size = new System.Drawing.Size(154, 20);
+            this.dtpFechaConsumo.Size = new System.Drawing.Size(204, 22);
             this.dtpFechaConsumo.TabIndex = 66;
             // 
             // cboxCodigoServicio
             // 
             this.cboxCodigoServicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboxCodigoServicio.FormattingEnabled = true;
-            this.cboxCodigoServicio.Location = new System.Drawing.Point(271, 170);
+            this.cboxCodigoServicio.Location = new System.Drawing.Point(361, 209);
+            this.cboxCodigoServicio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cboxCodigoServicio.Name = "cboxCodigoServicio";
-            this.cboxCodigoServicio.Size = new System.Drawing.Size(156, 23);
+            this.cboxCodigoServicio.Size = new System.Drawing.Size(207, 26);
             this.cboxCodigoServicio.TabIndex = 64;
             // 
             // cboxCodigoReserva
             // 
             this.cboxCodigoReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboxCodigoReserva.FormattingEnabled = true;
-            this.cboxCodigoReserva.Location = new System.Drawing.Point(271, 98);
+            this.cboxCodigoReserva.Location = new System.Drawing.Point(361, 121);
+            this.cboxCodigoReserva.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cboxCodigoReserva.Name = "cboxCodigoReserva";
-            this.cboxCodigoReserva.Size = new System.Drawing.Size(156, 23);
+            this.cboxCodigoReserva.Size = new System.Drawing.Size(207, 26);
             this.cboxCodigoReserva.TabIndex = 58;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Calisto MT", 14F);
-            this.label7.Location = new System.Drawing.Point(666, 100);
+            this.label7.Location = new System.Drawing.Point(888, 123);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(146, 22);
+            this.label7.Size = new System.Drawing.Size(185, 28);
             this.label7.TabIndex = 22;
             this.label7.Text = "Fecha Consumo:";
             // 
@@ -175,27 +189,30 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calisto MT", 14F);
-            this.label3.Location = new System.Drawing.Point(666, 36);
+            this.label3.Location = new System.Drawing.Point(888, 44);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 22);
+            this.label3.Size = new System.Drawing.Size(88, 28);
             this.label3.TabIndex = 20;
             this.label3.Text = "Monto:";
             // 
             // txtCodigoConsumo
             // 
-            this.txtCodigoConsumo.Location = new System.Drawing.Point(271, 37);
+            this.txtCodigoConsumo.Location = new System.Drawing.Point(361, 46);
+            this.txtCodigoConsumo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCodigoConsumo.Name = "txtCodigoConsumo";
             this.txtCodigoConsumo.ReadOnly = true;
-            this.txtCodigoConsumo.Size = new System.Drawing.Size(156, 20);
+            this.txtCodigoConsumo.Size = new System.Drawing.Size(207, 22);
             this.txtCodigoConsumo.TabIndex = 15;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Calisto MT", 14F);
-            this.label5.Location = new System.Drawing.Point(666, 177);
+            this.label5.Location = new System.Drawing.Point(888, 218);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 22);
+            this.label5.Size = new System.Drawing.Size(89, 28);
             this.label5.TabIndex = 7;
             this.label5.Text = "Estado:";
             // 
@@ -203,9 +220,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calisto MT", 14F);
-            this.label4.Location = new System.Drawing.Point(98, 172);
+            this.label4.Location = new System.Drawing.Point(131, 212);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(143, 22);
+            this.label4.Size = new System.Drawing.Size(183, 28);
             this.label4.TabIndex = 5;
             this.label4.Text = "Codigo Servicio:";
             // 
@@ -213,9 +231,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calisto MT", 14F);
-            this.label2.Location = new System.Drawing.Point(98, 100);
+            this.label2.Location = new System.Drawing.Point(131, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(143, 22);
+            this.label2.Size = new System.Drawing.Size(181, 28);
             this.label2.TabIndex = 2;
             this.label2.Text = "Codigo Reserva:";
             // 
@@ -223,22 +242,30 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calisto MT", 14F);
-            this.label1.Location = new System.Drawing.Point(98, 36);
+            this.label1.Location = new System.Drawing.Point(131, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 22);
+            this.label1.Size = new System.Drawing.Size(198, 28);
             this.label1.TabIndex = 1;
             this.label1.Text = "Codigo Consumo:";
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
             // frmVentasDetalle
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.ClientSize = new System.Drawing.Size(1685, 838);
             this.Controls.Add(this.dgvConsumos);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.lblFecha);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmVentasDetalle";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgvConsumos)).EndInit();
@@ -268,6 +295,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
